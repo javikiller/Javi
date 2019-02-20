5b55545c977cea06eb82dcf926704c9ea723634d
JaviKiller <javikiller5000@gmail.com>
2019-02-20 23:09:49 +0100

    Ejercicio

---
+++ mundo de pruebas/Assets/movimiento.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimiento : MonoBehaviour {
+
+    public float vel = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * vel);
+        }
+	}
+}
